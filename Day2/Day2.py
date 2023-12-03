cab08b478682bbfb2aceb3da7e7df46443428df4
Rob Eusanio <robeusanio11@gmail.com>
2023-12-02 16:48:06 -0800

    added time

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 '''
 --- Day 2: Cube Conundrum ---
 You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
@@ -25,6 +26,7 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 '''
+p1start = datetime.now()
 limits = {
     "red": 12,
     "green": 13,
@@ -47,7 +49,8 @@ for line in lines:
     if (possible):
         sum += int(gameNum)        
 print(sum)
-
+p1end = datetime.now()
+print(p1end-p1start)
 
 '''
 --- Part Two ---
