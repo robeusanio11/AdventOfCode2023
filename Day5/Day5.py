3406a058a22ee2b59ba5c5132b9a4d468e2d1f09
Rob Eusanio <robeusanio11@gmail.com>
2023-12-04 22:16:06 -0800

    part 1 completed

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -94,6 +94,89 @@ So, the lowest location number in this example is 35.
 
 What is the lowest location number that corresponds to any of the initial seed numbers?
 '''
+# f = open("AdventOfCode2023\Day5\seeds.txt", "r")
+# # seeds in arr
+# seeds = f.readline().split(":")[1].split()
+# lines = f.readlines()
+# locations = []
+# for seed in seeds:    
+#     curNum = int(seed)
+#     for line in lines:
+#         # find new maps
+#         if line[0].isalpha():
+#             newMap = True
+#             wasShifted = False
+#         else:
+#             if not line == '\n':
+#                 # run if not yet mapped
+#                 if newMap and not wasShifted:
+#                         # get source, destination, range
+#                         dst, src, rng = line.split()
+#                         dst = int(dst)
+#                         src = int(src)
+#                         rng = int(rng)
+#                         # if curNum is in line range - shift num and iterate until new map
+#                         if (curNum >= src and curNum <= src+rng):
+#                              wasShifted = True
+#                              newMap = False
+#                              curNum = (curNum-src)+dst
+#     locations.append(curNum)
+# print(min(locations))
+                    
+'''
+--- Part Two ---
+Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+'''
 f = open("AdventOfCode2023\Day5\seeds.txt", "r")
-seeds = f.readline().split(":")[1].split()
-print(seeds)
+seedsList = f.readline().split(":")[1].split()
+lines = f.readlines()
+locations = []
+start = True
+seeds = []
+starts = []
+ranges = []
+for seed in seedsList:
+    if start:
+        starts.append(int(seed))
+        start = not start
+    else:
+        ranges.append(int(seed))
+        start = not start
+for i in range(len(starts)):
+    for num in range(starts[i], starts[i]+ranges[i]):
+        seeds.append(num)
+
+for seed in seeds:    
+    curNum = int(seed)
+    for line in lines:
+        # find new maps
+        if line[0].isalpha():
+            newMap = True
+            wasShifted = False
+        else:
+            if not line == '\n':
+                # run if not yet mapped
+                if newMap and not wasShifted:
+                        # get source, destination, range
+                        dst, src, rng = line.split()
+                        dst = int(dst)
+                        src = int(src)
+                        rng = int(rng)
+                        # if curNum is in line range - shift num and iterate until new map
+                        if (curNum >= src and curNum <= src+rng):
+                             wasShifted = True
+                             newMap = False
+                             curNum = (curNum-src)+dst
+    locations.append(curNum)
+print(min(locations))
